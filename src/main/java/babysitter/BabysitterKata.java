82993fe2aaa1c3eac99768e44a7de991b5f0d0f6
Meghan Domeck <mdomeck@aol.com>
2021-04-14 22:14:28 -0400

    tests are passing

--- src/main/java/babysitter/BabysitterKata.java
+++ src/main/java/babysitter/BabysitterKata.java
@@ -2,28 +2,30 @@ package babysitter;
 
 public class BabysitterKata {
 
+    public static int income(int startTime, int bedTime, int endTime) throws Exception{
 
-
-//    int startTime= 5; // PM
-//    int bedTime = 6;  // PM
-//    int endTime = 4; // AM
-
-
-
-    public int income(int startTime, int bedTime, int endTime) {
+        if(startTime < 5 || startTime > 12){
+            throw new Exception("Please enter valid start time");
+        }
+        if(bedTime < startTime || bedTime > 12){
+            throw new Exception("Enter a valid bed time");
+        }
+        if(endTime >= 5 && endTime < bedTime){
+            throw new Exception("Enter a valid end time");
+        }
 
         int totalPay = 0;
 
         int firstHours = (bedTime - startTime) * 12;
         totalPay += firstHours;
 
-        if (endTime <= 12) {
-            int secondPay = (endTime - bedTime) * 8;
-            totalPay += secondPay;
-        } else {
+        if (endTime <= 4) {
             int secondPay = (12 - bedTime) * 8;
             int thirdPay = endTime * 16;
             totalPay += secondPay + thirdPay;
+        } else {
+            int secondPay = (endTime - bedTime) * 8;
+            totalPay += secondPay;
         }
         return totalPay;
     }
--- src/test/java/babysitter/BabysitterKataTest.java
+++ src/test/java/babysitter/BabysitterKataTest.java
@@ -1,5 +1,59 @@
 package babysitter;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+
 public class BabysitterKataTest {
+
+    @Test
+    public void payTestEndBeforeMidnight() throws Exception {
+        assertEquals(60, BabysitterKata.income(5, 6, 12));
+        assertEquals(48, BabysitterKata.income(6, 8, 11));
+        assertEquals(48, BabysitterKata.income(5, 7, 10));
+        assertEquals(40, BabysitterKata.income(7, 7, 12));
+    }
+
+    @Test
+    public void payTestSameStartAndBedTime() throws Exception {
+        assertEquals(56, BabysitterKata.income(5, 5, 12));
+        assertEquals(48, BabysitterKata.income(6, 6, 12));
+        assertEquals(40, BabysitterKata.income(7, 7, 12));
+        assertEquals(32, BabysitterKata.income(8, 8, 12));
+    }
+
+    @Test
+    public void payTestLateNight() throws Exception {
+        assertEquals(124, BabysitterKata.income(5, 6, 4));
+        assertEquals(108, BabysitterKata.income(5, 6, 3));
+        assertEquals(80, BabysitterKata.income(6, 6, 2));
+        assertEquals(60, BabysitterKata.income(7, 8, 1));
+    }
+
+    @Test
+    public void noPay() throws Exception {
+        assertEquals(0, BabysitterKata.income(5, 5, 5));
+        assertEquals(0, BabysitterKata.income(12, 12, 12));
+    }
+
+    @Test
+    public void notValidStartTimes() throws Exception {
+        assertThrows(Exception.class, () -> BabysitterKata.income(3, 5, 12));
+        assertThrows(Exception.class, () -> BabysitterKata.income(13, 5, 12));
+    }
+
+    @Test
+    public void notValidBedTimes() throws Exception {
+        assertThrows(Exception.class, () -> BabysitterKata.income(6, 5, 12));
+        assertThrows(Exception.class, () -> BabysitterKata.income(7, 13, 1));
+    }
+
+    @Test
+    public void notValidEndTimes() throws Exception {
+        assertThrows(Exception.class, () -> BabysitterKata.income(6, 8, 5));
+        assertThrows(Exception.class, () -> BabysitterKata.income(6, 8, 7));
+    }
 
 }
