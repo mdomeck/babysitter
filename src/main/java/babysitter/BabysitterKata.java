8c19b4f364fb9bebdadca0adb227649202836c69
Meghan Domeck <mdomeck@aol.com>
2021-04-15 14:29:36 -0400

    readme updated, testing done

--- src/main/java/babysitter/BabysitterKata.java
+++ src/main/java/babysitter/BabysitterKata.java
@@ -4,14 +4,30 @@ public class BabysitterKata {
 
     public static int income(int startTime, String startTimeAMorPM, int bedTime, String bedTimeAMorPM, int endTime, String endTimeAMorPM) throws Exception{
 
-        if(startTime < 5 || startTime >= 13 || startTime == 12 && !(startTimeAMorPM.equalsIgnoreCase("AM")) || startTime < 12 && startTimeAMorPM.equalsIgnoreCase("AM")){
+        if(startTime < 5 ||
+                startTime >= 13 ||
+                startTime == 12 && !(startTimeAMorPM.equalsIgnoreCase("AM")) ||
+                startTime < 12 && startTimeAMorPM.equalsIgnoreCase("AM")) {
             throw new Exception("Please enter valid start time");
         }
-        if(bedTime < startTime || bedTime >= 13 || bedTime == 12 && !(bedTimeAMorPM.equalsIgnoreCase("AM")) || bedTime < 12 && bedTimeAMorPM.equalsIgnoreCase("AM")){
+
+        if(bedTime < startTime ||
+                bedTime >= 13 ||
+                bedTime == 12 && !(bedTimeAMorPM.equalsIgnoreCase("AM")) ||
+                bedTime < 12 && bedTimeAMorPM.equalsIgnoreCase("AM")) {
             throw new Exception("Enter a valid bed time");
         }
-        if(endTime == 5 || endTime >= 13 || endTime == 12 && endTimeAMorPM.equalsIgnoreCase("PM") || endTime < 5 && endTimeAMorPM.equalsIgnoreCase("PM")|| endTime < bedTime && !(endTimeAMorPM.equalsIgnoreCase("AM"))) {
+
+        if(endTime == 5 ||
+                endTime >= 13 ||
+                endTime == 12 && endTimeAMorPM.equalsIgnoreCase("PM") ||
+                endTime < 6 && endTimeAMorPM.equalsIgnoreCase("PM")||
+                endTime < bedTime && !(endTimeAMorPM.equalsIgnoreCase("AM"))) {
             throw new Exception("Enter a valid end time");
+        }
+
+        if(startTime == endTime) {
+            throw new Exception("Start time and End time must be different");
         }
 
         int totalPay = 0;
--- src/test/java/babysitter/BabysitterKataTest.java
+++ src/test/java/babysitter/BabysitterKataTest.java
@@ -12,8 +12,7 @@ public class BabysitterKataTest {
     public void payTestEndBeforeMidnight() throws Exception {
         assertEquals(60, BabysitterKata.income(5,"PM", 6,"PM", 12, "AM"));
         assertEquals(48, BabysitterKata.income(6,"PM",8, "PM", 11, "PM"));
-        assertEquals(48, BabysitterKata.income(5, "PM", 7, "PM", 10, "PM"));
-        assertEquals(40, BabysitterKata.income(7, "PM", 7, "PM", 12, "AM"));
+        assertEquals(52, BabysitterKata.income(5, "PM", 8, "PM", 10, "PM"));
     }
 
     @Test
@@ -25,6 +24,13 @@ public class BabysitterKataTest {
     }
 
     @Test
+    public void payTestSameBedTimeAndEndTime() throws Exception {
+        assertEquals(84, BabysitterKata.income(5, "PM", 12, "AM", 12, "AM"));
+        assertEquals(24, BabysitterKata.income(6, "PM", 8, "PM", 8, "PM"));
+        assertEquals(36, BabysitterKata.income(7, "PM", 10, "PM", 10, "PM"));
+    }
+
+    @Test
     public void payTestLateNight() throws Exception {
         assertEquals(124, BabysitterKata.income(5, "PM", 6, "PM", 4, "AM"));
         assertEquals(108, BabysitterKata.income(5, "PM", 6, "PM", 3, "AM"));
@@ -33,13 +39,7 @@ public class BabysitterKataTest {
     }
 
     @Test
-    public void noPay() throws Exception {
-        assertEquals(0, BabysitterKata.income(6, "PM", 6, "PM", 6, "PM"));
-        assertEquals(0, BabysitterKata.income(12, "AM", 12, "AM", 12, "AM"));
-    }
-
-    @Test
-    public void notValidStartTimes() throws Exception {
+    public void notValidStartTimes() {
         assertThrows(Exception.class, () -> BabysitterKata.income(7, "AM", 5, "PM", 12, "AM"));
         assertThrows(Exception.class, () -> BabysitterKata.income(10, "AM", 5, "PM", 12, "AM"));
         assertThrows(Exception.class, () -> BabysitterKata.income(3, "PM", 5, "PM", 12, "AM"));
@@ -48,21 +48,27 @@ public class BabysitterKataTest {
     }
 
     @Test
-    public void notValidBedTimes() throws Exception {
+    public void notValidBedTimes() {
         assertThrows(Exception.class, () -> BabysitterKata.income(7, "PM", 5, "PM", 4, "AM"));
         assertThrows(Exception.class, () -> BabysitterKata.income(5, "PM", 13, "PM", 4, "AM"));
-        assertThrows(Exception.class, () -> BabysitterKata.income(12, "PM", 12, "PM", 4, "AM"));
-        assertThrows(Exception.class, () -> BabysitterKata.income(5, "PM", 10, "PM", 9, "PM"));
-        assertThrows(Exception.class, () -> BabysitterKata.income(5, "PM", 11, "PM", 10, "PM"));
+        assertThrows(Exception.class, () -> BabysitterKata.income(5, "PM", 12, "PM", 4, "AM"));
+        assertThrows(Exception.class, () -> BabysitterKata.income(12, "PM", 10, "AM", 4, "AM"));
     }
 
     @Test
-    public void notValidEndTimes() throws Exception {
+    public void notValidEndTimes() {
         assertThrows(Exception.class, () -> BabysitterKata.income(5, "PM", 5, "PM", 5, "AM"));
         assertThrows(Exception.class, () -> BabysitterKata.income(5, "PM", 5, "PM", 13, "PM"));
         assertThrows(Exception.class, () -> BabysitterKata.income(5, "PM", 5, "PM", 12, "PM"));
         assertThrows(Exception.class, () -> BabysitterKata.income(5, "PM", 9, "PM", 4, "PM"));
         assertThrows(Exception.class, () -> BabysitterKata.income(5, "PM", 9, "PM", 8, "PM"));
+    }
+
+    @Test
+    public void noPayException() {
+        assertThrows(Exception.class, () -> BabysitterKata.income(6, "PM", 6, "PM", 6, "PM"));
+        assertThrows(Exception.class, () -> BabysitterKata.income(7, "PM", 7, "PM", 7, "PM"));
+        assertThrows(Exception.class, () -> BabysitterKata.income(12, "PM", 12, "PM", 12, "PM"));
     }
 
 }
